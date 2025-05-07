0f6bfcb3b3c7c1ede2cb7b05748d3aa275bd68b4
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2025-05-07 10:13:43 -0400

    Added react bootstrap formatting and styling to the navbar component Added header place holder to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router";
+import { Container, Row, Col, Navbar as BootstrapNavbar } from 'react-bootstrap';
 import Navbar from './components/navbar';
 import Jobs from './pages/jobs';
 import EmployeeManagement from './pages/employees';
@@ -8,17 +9,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <div className="content">
-        <Routes>
-          <Route path="/jobs" element={<Jobs />} />
-          <Route path="/employee" element={<EmployeeManagement />} />
-          <Route path="/customers" element={<CustomerManagement />} />
-          <Route path="/deptPositions" element={<DeptPosition />} />
-        </Routes>
-      </div>
-    </>
+    <div className="vh-100 d-flex flex-column">
+      {/* Header */}
+      <BootstrapNavbar bg="dark" variant="dark" className="p-3">
+        <Container fluid>
+          <BootstrapNavbar.Brand>Job Plan</BootstrapNavbar.Brand>
+        </Container>
+      </BootstrapNavbar>
+
+      {/* Main content area with sidebar */}
+      <Container fluid className="flex-grow-1">
+        <Row className="h-100">
+          {/* Sidebar Navigation */}
+          <Col md={3} lg={2} className="bg-light p-0 min-vh-100">
+            <Navbar />
+          </Col>
+
+          {/* Main Content Area */}
+          <Col md={9} lg={10} className="p-4">
+            <Routes>
+              <Route path="/jobs" element={<Jobs />} />
+              <Route path="/employee" element={<EmployeeManagement />} />
+              <Route path="/customers" element={<CustomerManagement />} />
+              <Route path="/deptPositions" element={<DeptPosition />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router";
+import { Nav } from "react-bootstrap";
 
 function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li><Link to="/jobs">Jobs</Link></li>
-                <li><Link to="/employee">Employee Management</Link></li>
-                <li><Link to="/customers">Customer Management</Link></li>
-                <li><Link to="/deptPositions">Details</Link></li>
-            </ul>
-        </nav>
+        <Nav className="flex-column bg-light p-0">
+            <Nav.Link as={Link} to="/jobs" className="p-3 border-bottom">
+                Jobs
+            </Nav.Link>
+            <Nav.Link as={Link} to="/employee" className="p-3 border-bottom">
+                Employee Management
+            </Nav.Link>
+            <Nav.Link as={Link} to="/customers" className="p-3 border-bottom">
+                Customer Management
+            </Nav.Link>
+            <Nav.Link as={Link} to="/deptPositions" className="p-3 border-bottom">
+                Details
+            </Nav.Link>
+        </Nav>
     );
 }
+
 export default Navbar;
